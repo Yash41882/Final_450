dee9a1216b122891e31a26862cf3a0a5c362e8ee
Yash Sharma <76611733+Yash41882@users.noreply.github.com>
2022-03-01 17:45:15 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -25,9 +25,10 @@ int revArr(int arr[],int size)
 
 int main()
 {
-    int arr[5],size;
+    int size;
     cout<<"\nEnter the size of Array = ";
     cin>>size;
+    int arr[size];
     for(int i=0;i<size;i++)
     {
        cin>>arr[i];
